7d54ceb8de2db2ec228af47f68f01fe00ad0225d
LuJunNan <1106209907@qq.com>
2023-10-24 23:28:49 +0800

    54 实名认证静态搭建

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,10 +1,13 @@
 import request from "@/utils/request";
-import type {CancelOrdeData, OrderResponseData, SubmitOrder} from "./type"
+import type {CancelOrdeData, OrderResponseData, SubmitOrder, UserInfoData} from "./type"
 enum API{
     // 提交订单，获取订单号码接口
    SUBMITORDER_URL="/order/orderInfo/auth/submitOrder/",
    GETORDERINFO_URL = "/order/orderInfo/auth/getOrderInfo/",
-   ORDERCANCEL_URL = "/order/orderInfo/auth/cancelOrder/"
+   ORDERCANCEL_URL = "/order/orderInfo/auth/cancelOrder/",
+   QRCODE_URL = "/order/weixin/createNative/",
+   //获取当前账号用户信息
+   USERINFO_URL = "/user/auth/getUserInfo"
 }
 // 提交订单
 export const reqSubmitOrder = (hoscode:string,scheduleId:string,patientId:number)=>request.post<any,SubmitOrder>(API.SUBMITORDER_URL+`/${hoscode}/${scheduleId}/${patientId}`)
@@ -12,3 +15,7 @@ export const reqSubmitOrder = (hoscode:string,scheduleId:string,patientId:number
 export const reqOrderInfo = (id:string)=>request.get<any,OrderResponseData>(API.GETORDERINFO_URL+id)
 // 取消订单
 export const reqCancelOrder = (id:string)=>request.get<any,CancelOrdeData>(API.ORDERCANCEL_URL+id)
+// 获取支付订单二维码接口
+export const reqQrcode = (orderId:string)=>request.get(API.QRCODE_URL+orderId)
+//获取当前账号用户信息
+export const reqUserInfo = ()=>request.get<any,UserInfoData>(API.USERINFO_URL)
--- src/api/user/type.ts
+++ src/api/user/type.ts
@@ -1,20 +1,20 @@
-export interface ResponseData{
+export interface ResponseData {
     "code": number,
     "message": string,
     "ok": boolean
 }
 // 提交订单接口，返回的数据的ts类型
-export interface SubmitOrder extends ResponseData{
-    data:string
+export interface SubmitOrder extends ResponseData {
+    data: string
 }
 // 订单详情数据ts类型
-export interface OrderInfo{
+export interface OrderInfo {
     "id": number,
     "createTime": string,
     "updateTime": string,
     "isDeleted": number,
     "param": {
-      "orderStatusString": string
+        "orderStatusString": string
     },
     "userId": number,
     "outTradeNo": string,
@@ -38,9 +38,30 @@ export interface OrderInfo{
     "orderStatus": number
 }
 export interface OrderResponseData extends ResponseData {
-    data:OrderInfo
+    data: OrderInfo
 }
 // 取消订单返回数据ts类型
-export interface CancelOrdeData extends ResponseData{
-    data:boolean
+export interface CancelOrdeData extends ResponseData {
+    data: boolean
+}
+
+// 就诊人信息
+export interface UserInfo {    
+    "id": number,
+    "createTime": string,
+    "updateTime": string,
+    "isDeleted": number,
+    "param": {},
+    "openid": null,
+    "nickName": null,
+    "phone": string,
+    "name": string,
+    "certificatesType": string,
+    "certificatesNo": string,
+    "certificatesUrl": string,
+    "authStatus": number,
+    "status": number
+}
+export interface UserInfoData extends ResponseData{
+    data:UserInfo
 }
